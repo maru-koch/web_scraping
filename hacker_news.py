aa46ad3d1cf07552980bdd35230dde01971442f3
maru-koch <nwokochafranklyn@gmail.com>
2022-03-03 19:40:37 +0100

    do loop

--- hacker_news.py
+++ hacker_news.py
@@ -15,13 +15,16 @@ import requests
 response = requests.get("https://news.ycombinator.com/news")
 
 #: soup object
-soup = BeautifulSoup(response.text, 'html.parser')
-scores = soup.find_all('a')
+soup = BeautifulSoup(response.text, 'html.parser') 
 links = soup.select('.storylink')
-votes = soup.select('score')
-
-def hack_news(links, vote):
-    for idx, _ in enumerate(links, start = 1):
+votes = soup.select('.score')
+def hack_news(links, votes):
+    hn = []
+    for idx, item in enumerate(links):
         title = links[idx].getText()
-        href = links[idx].get('href')
+        href = links[idx].get('href', None)
+        points = votes[idx].getText()
+        hn.append({'title': title, 'href': href})
+    return hn
 
+print(hack_news(links, votes))
